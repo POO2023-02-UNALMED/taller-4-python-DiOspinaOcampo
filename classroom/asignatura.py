2fee1d64190acfd40deddf21a5098968f2a24431
DiOspinaOcampo <jospinaoc@unal.edu.co>
2023-09-02 21:13:43 -0500

    COMMIT

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,8 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon = None):
         self._nombre = nombre
-        self._salon = salon
+        self._salon = "remoto" if not salon else salon
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return f"{self._nombre} {self._salon}"
